366f539dc75eecef63fd4002ec4d261bd3732f48
Pnilay <pnilay926@gmail.com>
2026-06-27 22:13:20 -0400

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import type { View } from "./data/constants";
@@ -8,20 +8,159 @@ import GroupsView from "./components/GroupsView";
 import AnalyticsView from "./components/AnalyticsView";
 import ExpensesView from "./components/ExpensesView";
 import FriendsView from "./components/FriendsView";
+import GroupDetailView from "./components/GroupDetailView";
+import ExpenseModal from "./components/ExpenseModal";
+import type {
+  PopulatedGroup,
+  GroupDTO,
+  UserDTO,
+  ExpenseDTO,
+  UserBalanceSummaryDTO,
+  RelatedUserDTO,
+} from "./data/types";
+import { groupService } from "./services/groupService";
+import { expenseService } from "./services/expenseService";
+import { userService } from "./services/userService";
+import FriendDetailView from "./components/FriendDetailView";
 
 function App() {
   const [view, setView] = useState<View>("dashboard");
+  const [selectedGroupId, setSelectedGroupId] = useState<number | null>(null);
+  const [showModal, setShowModal] = useState(false);
+  const [balanceSummary, setBalanceSummary] = useState<UserBalanceSummaryDTO | null>(null);
+  const [selectedFriend, setSelectedFriend] = useState<RelatedUserDTO | null>(null);
+  const [groups, setGroups] = useState<PopulatedGroup[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [friends, setFriends] = useState<RelatedUserDTO[]>([]);
+  const [error, setError] = useState(true);
 
+  const Base_Url = "http://localhost:8080/v1/api";
+
+  useEffect(() => {
+    const fetchGroups = async () => {
+      try {
+        setLoading(true);
+        const rawGroups = await groupService.getGroups();
+        const balanceSummary = await expenseService.getGlobalBalanceSummary(1);
+        const getFriends = await userService.getFriends(1);
+        setBalanceSummary(balanceSummary);
+        setFriends(getFriends);
+
+        const populatedGroupsData: PopulatedGroup[] = await Promise.all(
+          rawGroups.map(async (group) => {
+            try {
+              // Concurrent execution utilizing clean domain-driven service definitions
+              const [members, expenses] = await Promise.all([
+                groupService.getMembers(group.id),
+                expenseService.getGroupExpenses(group.id),
+              ]);
+
+              // Compute aggregate metrics safely
+              const balance = expenses.reduce((sum, exp) => sum + exp.amount, 0);
+
+              return {
+                ...group,
+                members,
+                expenses,
+                balance,
+              };
+            } catch (err) {
+              console.error(`Failed to load data details for group ${group.id}:`, err);
+              return { ...group, members: [], expenses: [], balance: 0 };
+            }
+          }),
+        );
+
+        setGroups(populatedGroupsData);
+      } catch (error: any) {
+        setError(error);
+        console.log("Error:: " + error);
+      } finally {
+        console.log(groups);
+        setLoading(false);
+      }
+    };
+    fetchGroups();
+  }, []);
+
+  function handleGroupClick(id: number): void {
+    setSelectedGroupId(id);
+    setView("group-detail");
+  }
+
+  const handleFriendClick = (friend: RelatedUserDTO) => {
+    setSelectedFriend(friend);
+    setView("friend-detail");
+  };
+
+  function handleOnAddExpenseClick(): void {
+    setShowModal(true);
+  }
+
+  function handleBackTo(prev: View): void {
+    setView(prev);
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-      <MainLayout sidebar={<Sidebar currentView={view} onNavigate={(v) => setView(v)} />}>
+      <MainLayout
+        sidebar={
+          <Sidebar
+            currentView={view}
+            onNavigate={(v) => setView(v)}
+            onAddExpense={() => setShowModal(true)}
+          />
+        }
+      >
         {/* Conditional View Router Mapping */}
-        {view === "dashboard" && <DashboardView />}
-        {view === "groups" && <GroupsView />}
+        {view === "dashboard" && (
+          <DashboardView
+            onAddExpense={handleOnAddExpenseClick}
+            onGroupClick={handleGroupClick}
+            balanceSummary={balanceSummary!}
+            expenses={groups.flatMap((group) =>
+              group.expenses.map((expense) => ({
+                ...expense,
+              })),
+            )}
+          />
+        )}
+        {view === "groups" && (
+          <GroupsView onGroupClick={handleGroupClick} groups={groups} loading={loading} />
+        )}
+        {view === "group-detail" && selectedGroupId && (
+          <GroupDetailView
+            onAddExpense={handleOnAddExpenseClick}
+            group={groups.find((g) => g.id === selectedGroupId)!}
+            onBack={handleBackTo}
+          />
+        )}
         {view === "expenses" && <ExpensesView />}
-        {view === "friends" && <FriendsView />}
+        {view === "friends" && (
+          <FriendsView friends={friends} groups={groups} onFriendClick={handleFriendClick} />
+        )}
+        {view === "friend-detail" && selectedFriend && (
+          <FriendDetailView
+            friend={selectedFriend}
+            // onAddExpense={handleOnAddExpenseClick}
+            // group={groups.find((g) => g.id === selectedGroupId)!}
+            onBack={handleBackTo}
+          />
+        )}
         {view === "analytics" && <AnalyticsView />}
       </MainLayout>
+
+      {showModal && (
+        <ExpenseModal
+          onClose={() => setShowModal(false)}
+          initialGroupId={selectedGroupId ?? undefined}
+          group={groups.find((g) => g.id === selectedGroupId)!}
+          friends={friends}
+        />
+      )}
     </>
   );
 }
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,13 +1,20 @@
 import { AVATAR_COLORS } from "../data/constants";
-import type { User } from "../data/constants";
+import type { UserDTO } from "../data/types";
+import { getColorIndex, getInitials } from "../utils/helper";
 
-export default function Avatar({ user, size = "md" }: { user: User; size?: "sm" | "md" | "lg" }) {
+export default function Avatar({
+  user,
+  size = "md",
+}: {
+  user: UserDTO;
+  size?: "sm" | "md" | "lg";
+}) {
   const sz = { sm: "w-7 h-7 text-xs", md: "w-9 h-9 text-sm", lg: "w-12 h-12 text-base" }[size];
   return (
     <div
-      className={`${sz} ${AVATAR_COLORS[user.colorIdx % AVATAR_COLORS.length]} rounded-full flex items-center justify-center font-bold text-white shrink-0 select-none`}
+      className={`${sz} ${AVATAR_COLORS[getColorIndex(user.id, AVATAR_COLORS.length)]} rounded-full flex items-center justify-center font-bold text-white shrink-0 select-none`}
     >
-      {user.initials}
+      {getInitials(user.name)}
     </div>
   );
 }
